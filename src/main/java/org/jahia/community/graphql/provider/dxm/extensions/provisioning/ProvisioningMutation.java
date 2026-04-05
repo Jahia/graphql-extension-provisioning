41fb050ad10f545d0faa0d932aedd40cde567d44
Florent BOURASSE <fbourasse@jahia.com>
2026-04-05 11:29:31 +0200

    Limit access to this graphql mutation

--- src/main/java/org/jahia/community/graphql/provider/dxm/extensions/provisioning/ProvisioningMutation.java
+++ src/main/java/org/jahia/community/graphql/provider/dxm/extensions/provisioning/ProvisioningMutation.java
@@ -5,6 +5,7 @@ import graphql.annotations.annotationTypes.GraphQLField;
 import graphql.annotations.annotationTypes.GraphQLName;
 import graphql.annotations.annotationTypes.GraphQLTypeExtension;
 import org.jahia.modules.graphql.provider.dxm.admin.GqlJahiaAdminMutation;
+import org.jahia.modules.graphql.provider.dxm.security.GraphQLRequiresPermission;
 import org.jahia.osgi.BundleUtils;
 import org.jahia.services.provisioning.ProvisioningManager;
 import org.slf4j.Logger;
@@ -17,6 +18,7 @@ public class ProvisioningMutation {
 
     @GraphQLField
     @GraphQLDescription("Execute a YAML provisioning script provided as a string")
+    @GraphQLRequiresPermission("provisioningApi")
     public static Boolean executeScript(
             @GraphQLName("script") @GraphQLDescription("YAML provisioning script content") String script
     ) {
