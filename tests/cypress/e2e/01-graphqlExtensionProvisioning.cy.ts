544e5d5bd9cbee6e06df49be38f8b30efd0d38e0
Florent BOURASSE <fbourasse@jahia.com>
2026-04-15 20:10:42 +0200

    correction linx reported issues related to Cypress tests
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- tests/cypress/e2e/01-graphqlExtensionProvisioning.cy.ts
+++ tests/cypress/e2e/01-graphqlExtensionProvisioning.cy.ts
@@ -1,44 +1,42 @@
-import {DocumentNode} from 'graphql'
+import {DocumentNode} from 'graphql';
 
 describe('GraphQL Extension Provisioning', () => {
-    let executeScript: DocumentNode
-
-    executeScript = require('graphql-tag/loader!../fixtures/graphql/mutation/executeScript.graphql')
-
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const executeScript: DocumentNode = require('graphql-tag/loader!../fixtures/graphql/mutation/executeScript.graphql');
     before(() => {
-        cy.login()
-    })
+        cy.login();
+    });
 
     it('executes a valid YAML provisioning script and returns true', () => {
-        const script = "- karafCommand: \"log:log 'graphql-extension-provisioning cypress test'\""
+        const script = '- karafCommand: "log:log \'graphql-extension-provisioning cypress test\'"';
         cy.apollo({
             mutation: executeScript,
             variables: {script}
         })
             .its('data.admin.jahia.executeScript')
-            .should('eq', true)
-    })
+            .should('eq', true);
+    });
 
     it('returns false when executing an invalid YAML script', () => {
-        const script = '{ invalid yaml: [unclosed'
+        const script = '{ invalid yaml: [unclosed';
         cy.apollo({
             mutation: executeScript,
             variables: {script}
         })
             .its('data.admin.jahia.executeScript')
-            .should('eq', false)
-    })
+            .should('eq', false);
+    });
 
     it('executes a multi-step YAML provisioning script and returns true', () => {
         const script = [
-            "- karafCommand: \"log:log 'START - cypress multi-step test'\"",
-            "- karafCommand: \"log:log 'END - cypress multi-step test'\""
-        ].join('\n')
+            '- karafCommand: "log:log \'START - cypress multi-step test\'"',
+            '- karafCommand: "log:log \'END - cypress multi-step test\'"'
+        ].join('\n');
         cy.apollo({
             mutation: executeScript,
             variables: {script}
         })
             .its('data.admin.jahia.executeScript')
-            .should('eq', true)
-    })
-})
+            .should('eq', true);
+    });
+});
