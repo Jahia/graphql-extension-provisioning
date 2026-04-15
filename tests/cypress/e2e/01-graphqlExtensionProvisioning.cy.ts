372c4f47d1f38b718c31b094f8522f0251e368e7
Florent BOURASSE <fbourasse@jahia.com>
2026-04-15 19:52:25 +0200

    Addition of Cypress Docker-based tests
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

---
+++ tests/cypress/e2e/01-graphqlExtensionProvisioning.cy.ts
@@ -0,0 +1,44 @@
+import {DocumentNode} from 'graphql'
+
+describe('GraphQL Extension Provisioning', () => {
+    let executeScript: DocumentNode
+
+    executeScript = require('graphql-tag/loader!../fixtures/graphql/mutation/executeScript.graphql')
+
+    before(() => {
+        cy.login()
+    })
+
+    it('executes a valid YAML provisioning script and returns true', () => {
+        const script = "- karafCommand: \"log:log 'graphql-extension-provisioning cypress test'\""
+        cy.apollo({
+            mutation: executeScript,
+            variables: {script}
+        })
+            .its('data.admin.jahia.executeScript')
+            .should('eq', true)
+    })
+
+    it('returns false when executing an invalid YAML script', () => {
+        const script = '{ invalid yaml: [unclosed'
+        cy.apollo({
+            mutation: executeScript,
+            variables: {script}
+        })
+            .its('data.admin.jahia.executeScript')
+            .should('eq', false)
+    })
+
+    it('executes a multi-step YAML provisioning script and returns true', () => {
+        const script = [
+            "- karafCommand: \"log:log 'START - cypress multi-step test'\"",
+            "- karafCommand: \"log:log 'END - cypress multi-step test'\""
+        ].join('\n')
+        cy.apollo({
+            mutation: executeScript,
+            variables: {script}
+        })
+            .its('data.admin.jahia.executeScript')
+            .should('eq', true)
+    })
+})
